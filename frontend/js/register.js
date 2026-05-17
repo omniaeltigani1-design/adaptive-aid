8d5530fc5f5376430d99f0f1936537970d529093
Omnia Eltigani <omniaeltigani1@gmail.com>
2026-05-17 17:27:05 +0300

    make cards same size

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://corrosive-tipper-daydream.ngrok-free.dev";
 const REGISTER_ENDPOINT = `${API_BASE_URL}/auth/register`;
 
 document.addEventListener("DOMContentLoaded", () => {
